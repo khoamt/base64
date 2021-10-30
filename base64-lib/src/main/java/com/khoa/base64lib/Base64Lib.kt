f246496e013eefc7194165a62f60cf2e4235d68f
Khoa Truong <khoa.truong@nexlab.tech>
2021-10-30 17:54:42 +0700

    First commit

---
+++ base64-lib/src/main/java/com/khoa/base64lib/Base64Lib.kt
@@ -0,0 +1,14 @@
+package com.khoa.base64lib
+
+import android.util.Base64
+
+class Base64Lib {
+    companion object {
+        fun encode(value: String): String {
+            return Base64.encodeToString(value.toByteArray(), Base64.NO_WRAP)
+        }
+    }
+}
+
+val String.base64
+    get() = Base64Lib.encode(this)
